805c46369a2d885ff900bb494301d53b5610f3a9
look4pritam <pritam.shete@gmail.com>
2018-05-10 11:33:42 +0530

    Sample multiplier factor is added.

--- datasets/LandmarkDataset.py
+++ datasets/LandmarkDataset.py
@@ -95,14 +95,17 @@ class LandmarkDataset(object):
 		landmark_file = open(LandmarkDataset.landmark_file_name(target_root_dir), 'w')
 
 		size = minimum_face
-		augment = True
 
 		generated_landmark_images = 0
-
     		processed_input_images = 0
 		total_number_of_input_images = len(image_file_names)
 
-		for image_path, bounding_box, ground_truth_landmark in zip(image_file_names, ground_truth_boxes, ground_truth_landmarks):
+		needed_landmark_images = np.ceil( ( base_number_of_images * LandmarkDataset.__landmark_ratio ) / total_number_of_input_images )
+		base_number_of_attempts = 200
+		maximum_attempts = base_number_of_attempts * needed_landmark_images
+
+		for image_path, ground_truth_bounding_box, ground_truth_landmark in zip(image_file_names, ground_truth_boxes, ground_truth_landmarks):
+
         		F_imgs = []
         		F_landmarks = []  
 
@@ -112,8 +115,8 @@ class LandmarkDataset(object):
 				continue     
    		
         		image_height, image_width, image_channels = image.shape
-        		gt_box = np.array([bounding_box.left, bounding_box.top, bounding_box.right, bounding_box.bottom])
-        		f_face = image[bounding_box.top:bounding_box.bottom+1, bounding_box.left:bounding_box.right+1]
+        		gt_box = np.array([ground_truth_bounding_box.left, ground_truth_bounding_box.top, ground_truth_bounding_box.right, ground_truth_bounding_box.bottom])
+        		f_face = image[ground_truth_bounding_box.top:ground_truth_bounding_box.bottom+1, ground_truth_bounding_box.left:ground_truth_bounding_box.right+1]
         		f_face = cv2.resize(f_face,(size,size))
         		landmark = np.zeros((5, 2))
 
@@ -125,10 +128,11 @@ class LandmarkDataset(object):
         		F_landmarks.append(landmark.reshape(10))
         		landmark = np.zeros((5, 2))  
 
-			if( augment ):
-            			processed_input_images = processed_input_images + 1
-            			if( processed_input_images % 1000 == 0 ):
-                			print( '( %s / %s ) number of input images are processed.' % ( processed_input_images, total_number_of_input_images) )
+			current_landmark_images = 0
+			number_of_attempts = 0
+			while(	(current_landmark_images < needed_landmark_images) and (number_of_attempts < maximum_attempts) ):
+
+				number_of_attempts += 1
 
             			x1, y1, x2, y2 = gt_box
             			ground_truth_width = x2 - x1 + 1
@@ -136,72 +140,70 @@ class LandmarkDataset(object):
             			if (x1 < 0 )or (y1 < 0):
                 			continue
 
-				for i in range(10):
-
-                			bounding_box_size = npr.randint(int(min(ground_truth_width, ground_truth_height) * 0.8), np.ceil(1.25 * max(ground_truth_width, ground_truth_height)))
-                			delta_x = npr.randint(-ground_truth_width, ground_truth_width) * 0.2
-                			delta_y = npr.randint(-ground_truth_height, ground_truth_height) * 0.2
-                			nx1 = int(max(x1+ground_truth_width/2-bounding_box_size/2+delta_x, 0))
-                			ny1 = int(max(y1+ground_truth_height/2-bounding_box_size/2+delta_y, 0))
+               			bounding_box_size = npr.randint(int(min(ground_truth_width, ground_truth_height) * 0.8), np.ceil(1.25 * max(ground_truth_width, ground_truth_height)))
+               			delta_x = npr.randint(-ground_truth_width, ground_truth_width) * 0.2
+               			delta_y = npr.randint(-ground_truth_height, ground_truth_height) * 0.2
+               			nx1 = int(max(x1+ground_truth_width/2-bounding_box_size/2+delta_x, 0))
+               			ny1 = int(max(y1+ground_truth_height/2-bounding_box_size/2+delta_y, 0))
                 
-                			nx2 = nx1 + int(bounding_box_size)
-                			ny2 = ny1 + int(bounding_box_size)
-                			if( ( nx2 > image_width ) or ( ny2 > image_height ) ):
-                    				continue
+               			nx2 = nx1 + int(bounding_box_size)
+               			ny2 = ny1 + int(bounding_box_size)
+               			if( ( nx2 > image_width ) or ( ny2 > image_height ) ):
+               				continue
 
-                			crop_box = np.array([nx1,ny1,nx2,ny2])
-                			cropped_im = image[ny1:ny2+1,nx1:nx2+1,:]
-                			resized_im = cv2.resize(cropped_im, (size, size))
+               			crop_box = np.array([nx1,ny1,nx2,ny2])
+               			cropped_im = image[ny1:ny2+1,nx1:nx2+1,:]
+               			resized_im = cv2.resize(cropped_im, (size, size))
 
-                			current_IoU = IoU(crop_box, np.expand_dims(gt_box,0))
+               			current_IoU = IoU(crop_box, np.expand_dims(gt_box,0))
 
-					if( current_IoU >= DatasetFactory.positive_IoU() ):
-                    				F_imgs.append(resized_im)
+				if( current_IoU >= DatasetFactory.positive_IoU() ):               				
 
-                    				for index, one in enumerate(ground_truth_landmark):
-                        				landmark_point = ((one[0]-nx1)/bounding_box_size, (one[1]-ny1)/bounding_box_size)
-                        				landmark[index] = landmark_point
+               				for index, one in enumerate(ground_truth_landmark):
+                       				landmark_point = ((one[0]-nx1)/bounding_box_size, (one[1]-ny1)/bounding_box_size)
+                       				landmark[index] = landmark_point
 
-                    				F_landmarks.append(landmark.reshape(10))
-                    				landmark = np.zeros((5, 2))
-                    				landmark_ = F_landmarks[-1].reshape(-1,2)
-                    				bounding_box = BBox([nx1,ny1,nx2,ny2])   
+					F_imgs.append(resized_im)
+               				F_landmarks.append(landmark.reshape(10))
+               				landmark = np.zeros((5, 2))
+               				landmark_ = F_landmarks[-1].reshape(-1,2)
+               				bounding_box = BBox([nx1,ny1,nx2,ny2])   
 
-                    				#mirror                    
-                    				if( self._can_generate_sample() ):
-                        				face_flipped, landmark_flipped = flip(resized_im, landmark_)
-                        				face_flipped = cv2.resize(face_flipped, (size, size))
-                        				#c*h*w
-                        				F_imgs.append(face_flipped)
-                        				F_landmarks.append(landmark_flipped.reshape(10))
+               				#mirror                    
+               				if( self._can_generate_sample() ):
+                      				face_flipped, landmark_flipped = flip(resized_im, landmark_)
+                       				face_flipped = cv2.resize(face_flipped, (size, size))
+                       				#c*h*w
+                       				F_imgs.append(face_flipped)
+                       				F_landmarks.append(landmark_flipped.reshape(10))
 
-                    				#rotate
-                    				if( self._can_generate_sample() ):
-                        				face_rotated_by_alpha, landmark_rotated = rotate(image, bounding_box,bounding_box.reprojectLandmark(landmark_), 5)
-                        				#landmark_offset
-                        				landmark_rotated = bounding_box.projectLandmark(landmark_rotated)
-                        				face_rotated_by_alpha = cv2.resize(face_rotated_by_alpha, (size, size))
-                        				F_imgs.append(face_rotated_by_alpha)
-                        				F_landmarks.append(landmark_rotated.reshape(10))
+               				#rotate
+               				if( self._can_generate_sample() ):
+                       				face_rotated_by_alpha, landmark_rotated = rotate(image, bounding_box, bounding_box.reprojectLandmark(landmark_), 5)
+                       				#landmark_offset
+                       				landmark_rotated = bounding_box.projectLandmark(landmark_rotated)
+                       				face_rotated_by_alpha = cv2.resize(face_rotated_by_alpha, (size, size))
+                       				F_imgs.append(face_rotated_by_alpha)
+                       				F_landmarks.append(landmark_rotated.reshape(10))
                 
-                        				#flip
-                        				face_flipped, landmark_flipped = flip(face_rotated_by_alpha, landmark_rotated)
-                        				face_flipped = cv2.resize(face_flipped, (size, size))
-                        				F_imgs.append(face_flipped)
-                       					F_landmarks.append(landmark_flipped.reshape(10))       							
+                       				#flip
+                       				face_flipped, landmark_flipped = flip(face_rotated_by_alpha, landmark_rotated)
+                       				face_flipped = cv2.resize(face_flipped, (size, size))
+                       				F_imgs.append(face_flipped)
+               					F_landmarks.append(landmark_flipped.reshape(10))       							
                     
-                    				#inverse clockwise rotation
-                    				if( self._can_generate_sample() ):
-                        				face_rotated_by_alpha, landmark_rotated = rotate(image, bounding_box, bounding_box.reprojectLandmark(landmark_), -5)
-                        				landmark_rotated = bounding_box.projectLandmark(landmark_rotated)
-                        				face_rotated_by_alpha = cv2.resize(face_rotated_by_alpha, (size, size))
-                        				F_imgs.append(face_rotated_by_alpha)
-                        				F_landmarks.append(landmark_rotated.reshape(10))
+               				#inverse clockwise rotation
+               				if( self._can_generate_sample() ):
+                      				face_rotated_by_alpha, landmark_rotated = rotate(image, bounding_box, bounding_box.reprojectLandmark(landmark_), -5)
+                       				landmark_rotated = bounding_box.projectLandmark(landmark_rotated)
+                       				face_rotated_by_alpha = cv2.resize(face_rotated_by_alpha, (size, size))
+                       				F_imgs.append(face_rotated_by_alpha)
+                       				F_landmarks.append(landmark_rotated.reshape(10))
                 
-                        				face_flipped, landmark_flipped = flip(face_rotated_by_alpha, landmark_rotated)
-                        				face_flipped = cv2.resize(face_flipped, (size, size))
-                        				F_imgs.append(face_flipped)
-                        				F_landmarks.append(landmark_flipped.reshape(10)) 
+                       				face_flipped, landmark_flipped = flip(face_rotated_by_alpha, landmark_rotated)
+                       				face_flipped = cv2.resize(face_flipped, (size, size))
+                       				F_imgs.append(face_flipped)
+                       				F_landmarks.append(landmark_flipped.reshape(10)) 
 
 				F_imgs, F_landmarks = np.asarray(F_imgs), np.asarray(F_landmarks)
 
@@ -212,10 +214,18 @@ class LandmarkDataset(object):
                 			if np.sum(np.where(F_landmarks[i] >= 1, 1, 0)) > 0:
                     				continue
 
-                			cv2.imwrite(join(landmark_dir,"%d.jpg" %(generated_landmark_images)), F_imgs[i])
-                			landmarks = map(str,list(F_landmarks[i]))
-                			landmark_file.write(join(landmark_dir,"%d.jpg" %(generated_landmark_images))+" -2 "+" ".join(landmarks)+"\n")
-                			generated_landmark_images += 1
+					if(current_landmark_images < needed_landmark_images):
+                				cv2.imwrite(join(landmark_dir,"%d.jpg" %(generated_landmark_images)), F_imgs[i])
+                				landmarks = map(str,list(F_landmarks[i]))
+                				landmark_file.write(join(landmark_dir,"%d.jpg" %(generated_landmark_images))+" -2 "+" ".join(landmarks)+"\n")
+                				generated_landmark_images += 1
+						current_landmark_images += 1
+					else:
+						break
+
+       			processed_input_images = processed_input_images + 1
+       			if( processed_input_images % 1000 == 0 ):
+               			print( '( %s / %s ) number of input images are processed.' % ( processed_input_images, total_number_of_input_images) )
 
     		landmark_file.close()
 
