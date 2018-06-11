0a27cbd37f4eb576259b69a468a62edc3af26ea1
look4pritam <pritam.shete@gmail.com>
2018-06-11 11:33:53 +0530

    Script is modified.

--- scripts/train_wider_celeba_10.sh
+++ scripts/train_wider_celeba_10.sh
@@ -20,43 +20,43 @@ cd $MTCNN_ROOT_DIR/git-space/tfmtcnn/tfmtcnn
 
 echo 'Prepare CelebA dataset for input.'
 
-python tools/prepare_celeba_dataset.py --bounding_box_file_name=./data/CelebA/list_bbox_celeba.txt --landmark_file_name=./data/CelebA/list_landmarks_celeba.txt --output_file_name=./data/CelebA/CelebA.txt
+python tools/prepare_celeba_dataset.py --bounding_box_file_name=../data/CelebA/list_bbox_celeba.txt --landmark_file_name=../data/CelebA/list_landmarks_celeba.txt --output_file_name=../data/CelebA/CelebA.txt
 
 echo '########################################################################################################################################################################################################'
 
 echo 'Generate a basic dataset i.e. PNet dataset.'
 
-python generate_simple_dataset.py --annotation_image_dir=./data/WIDER_Face/WIDER_train/images --annotation_file_name=./data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=./data/CelebA/images --landmark_file_name=./data/CelebA/CelebA.txt --sample_multiplier_factor=10 --target_root_dir=./data/datasets/mtcnn
+python generate_simple_dataset.py --annotation_image_dir=../data/WIDER_Face/WIDER_train/images --annotation_file_name=../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=../data/CelebA/images --landmark_file_name=../data/CelebA/CelebA.txt --sample_multiplier_factor=10 --target_root_dir=../data/datasets/mtcnn
 
 echo '########################################################################################################################################################################################################'
 
 echo 'Train PNet.'
 
-python train_model.py --network_name=PNet --train_root_dir=./data/models/mtcnn/train --dataset_root_dir=./data/datasets/mtcnn --base_learning_rate=0.01 --max_number_of_epoch=30
+python train_model.py --network_name=PNet --train_root_dir=../data/models/mtcnn/train --dataset_root_dir=../data/datasets/mtcnn --base_learning_rate=0.01 --max_number_of_epoch=30
 
 echo '########################################################################################################################################################################################################'
 
 echo 'Generate a hard dataset i.e. RNet dataset.'
 
-python generate_hard_dataset.py --network_name=RNet --train_root_dir=./data/models/mtcnn/train --annotation_image_dir=./data/WIDER_Face/WIDER_train/images --annotation_file_name=./data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=./data/CelebA/images --landmark_file_name=./data/CelebA/CelebA.txt --target_root_dir=./data/datasets/mtcnn 
+python generate_hard_dataset.py --network_name=RNet --train_root_dir=../data/models/mtcnn/train --annotation_image_dir=../data/WIDER_Face/WIDER_train/images --annotation_file_name=../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=../data/CelebA/images --landmark_file_name=../data/CelebA/CelebA.txt --target_root_dir=../data/datasets/mtcnn 
 
 echo '########################################################################################################################################################################################################'
 
 echo 'Train RNet.'
 
-python train_model.py --network_name=RNet --train_root_dir=./data/models/mtcnn/train --dataset_root_dir=./data/datasets/mtcnn --base_learning_rate=0.01 --max_number_of_epoch=22
+python train_model.py --network_name=RNet --train_root_dir=../data/models/mtcnn/train --dataset_root_dir=../data/datasets/mtcnn --base_learning_rate=0.01 --max_number_of_epoch=22
 
 echo '########################################################################################################################################################################################################'
 
 echo 'Generate a hard dataset i.e. ONet dataset.'
 
-python generate_hard_dataset.py --network_name=ONet --train_root_dir=./data/models/mtcnn/train --annotation_image_dir=./data/WIDER_Face/WIDER_train/images --annotation_file_name=./data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=./data/CelebA/images --landmark_file_name=./data/CelebA/CelebA.txt --target_root_dir=./data/datasets/mtcnn 
+python generate_hard_dataset.py --network_name=ONet --train_root_dir=../data/models/mtcnn/train --annotation_image_dir=../data/WIDER_Face/WIDER_train/images --annotation_file_name=../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=../data/CelebA/images --landmark_file_name=../data/CelebA/CelebA.txt --target_root_dir=../data/datasets/mtcnn 
 
 echo '########################################################################################################################################################################################################'
 
 echo 'Train ONet.'
 
-python train_model.py --network_name=ONet --train_root_dir=./data/models/mtcnn/train --dataset_root_dir=./data/datasets/mtcnn --base_learning_rate=0.01 --max_number_of_epoch=22
+python train_model.py --network_name=ONet --train_root_dir=../data/models/mtcnn/train --dataset_root_dir=../data/datasets/mtcnn --base_learning_rate=0.01 --max_number_of_epoch=22
 
 echo '########################################################################################################################################################################################################'
 
