a4383321c2d2482b16cdb200806aeaa9d4d81a95
look4pritam <pritam.shete@gmail.com>
2018-08-13 14:53:44 +0530

    LFW landmark dataset is added.

--- scripts/train_wider_lfw.sh
+++ scripts/train_wider_lfw.sh
@@ -20,17 +20,9 @@ cd $MTCNN_ROOT_DIR/git-space/tfmtcnn/tfmtcnn
 
 echo '########################################################################################################################################################################################################'
 
-echo 'Prepare CelebA dataset for input.'
-
-python tools/prepare_celeba_dataset.py --bounding_box_file_name=../data/CelebA/list_bbox_celeba.txt --landmark_file_name=../data/CelebA/list_landmarks_celeba.txt --output_file_name=../data/CelebA/CelebA.txt
-
-echo 'Prepared CelebA dataset for input.'
-
-echo '########################################################################################################################################################################################################'
-
 echo 'Generate a basic dataset i.e. PNet dataset.'
 
-python generate_simple_dataset.py --annotation_image_dir=../data/WIDER_Face/WIDER_train/images --annotation_file_name=../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=../data/CelebA/images --landmark_file_name=../data/CelebA/CelebA.txt --base_number_of_images=700000 --target_root_dir=../data/datasets/mtcnn
+python generate_simple_dataset.py --annotation_image_dir=../data/WIDER_Face/WIDER_train/images --annotation_file_name=../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=../data/LFW_Landmark --landmark_file_name=../data/LFW_Landmark/trainImageList.txt --base_number_of_images=700000 --target_root_dir=../data/datasets/mtcnn
 
 echo 'Generated a basic dataset i.e. PNet dataset.'
 
@@ -46,7 +38,7 @@ echo '##########################################################################
 
 echo 'Generate a hard dataset i.e. RNet dataset.'
 
-python generate_hard_dataset.py --network_name=RNet --train_root_dir=../data/models/mtcnn/train --annotation_image_dir=../data/WIDER_Face/WIDER_train/images --annotation_file_name=../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=../data/CelebA/images --landmark_file_name=../data/CelebA/CelebA.txt --base_number_of_images=500000 --target_root_dir=../data/datasets/mtcnn 
+python generate_hard_dataset.py --network_name=RNet --train_root_dir=../data/models/mtcnn/train --annotation_image_dir=../data/WIDER_Face/WIDER_train/images --annotation_file_name=../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=../data/LFW_Landmark --landmark_file_name=../data/LFW_Landmark/trainImageList.txt --base_number_of_images=500000 --target_root_dir=../data/datasets/mtcnn 
 
 echo 'Generated a hard dataset i.e. RNet dataset.'
 
@@ -62,7 +54,7 @@ echo '##########################################################################
 
 echo 'Generate a hard dataset i.e. ONet dataset.'
 
-python generate_hard_dataset.py --network_name=ONet --train_root_dir=../data/models/mtcnn/train --annotation_image_dir=../data/WIDER_Face/WIDER_train/images --annotation_file_name=../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=../data/CelebA/images --landmark_file_name=../data/CelebA/CelebA.txt --base_number_of_images=500000 --target_root_dir=../data/datasets/mtcnn 
+python generate_hard_dataset.py --network_name=ONet --train_root_dir=../data/models/mtcnn/train --annotation_image_dir=../data/WIDER_Face/WIDER_train/images --annotation_file_name=../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir=../data/LFW_Landmark --landmark_file_name=../data/LFW_Landmark/trainImageList.txt --base_number_of_images=500000 --target_root_dir=../data/datasets/mtcnn 
 
 echo 'Generated a hard dataset i.e. ONet dataset.'
 
