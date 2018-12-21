46ea499c6a456afd683224f0b47e4f324b07e0cf
look4pritam <pritam.shete@gmail.com>
2018-12-21 17:03:47 +0530

    Code is formatted.

--- tfmtcnn/trainers/AbstractNetworkTrainer.py
+++ tfmtcnn/trainers/AbstractNetworkTrainer.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2018
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,45 +29,50 @@ import os
 from tfmtcnn.networks.NetworkFactory import NetworkFactory
 from tfmtcnn.datasets.TensorFlowDataset import TensorFlowDataset
 
+
 class AbstractNetworkTrainer(object):
+    def __init__(self, network_name):
+        self._network = NetworkFactory.network(network_name)
+        self._number_of_samples = 0
+        self._batch_size = 384
+        self._learning_rate_epoch = [8, 16, 24]
 
-	def __init__(self, network_name):
-		self._network = NetworkFactory.network(network_name)
-		self._number_of_samples = 0
-		self._batch_size = 384
-		self._learning_rate_epoch = [8, 16, 24]
+    def network_name(self):
+        return (self._network.network_name())
 
-	def network_name(self):
-		return(self._network.network_name())
+    def network_size(self):
+        return (self._network.network_size())
 
-	def network_size(self):
-		return(self._network.network_size())
-		
-	def dataset_dir(self, dataset_root_dir):
-		dataset_dir = os.path.join(dataset_root_dir, self.network_name())
-		tensorflow_dir = os.path.join(dataset_dir, 'tensorflow')
-		return(tensorflow_dir)
+    def dataset_dir(self, dataset_root_dir):
+        dataset_dir = os.path.join(dataset_root_dir, self.network_name())
+        tensorflow_dir = os.path.join(dataset_dir, 'tensorflow')
+        return (tensorflow_dir)
 
-	def network_train_dir(self, train_root_dir):
-		network_train_dir = os.path.join(train_root_dir, self.network_name())
-		return(network_train_dir)
+    def network_train_dir(self, train_root_dir):
+        network_train_dir = os.path.join(train_root_dir, self.network_name())
+        return (network_train_dir)
 
-	def _positive_file_name(self, dataset_dir):
-		positive_file_name = TensorFlowDataset.tensorflow_file_name(dataset_dir, 'positive')
-		return(positive_file_name)
+    def _positive_file_name(self, dataset_dir):
+        positive_file_name = TensorFlowDataset.tensorflow_file_name(
+            dataset_dir, 'positive')
+        return (positive_file_name)
 
-	def _part_file_name(self, dataset_dir):
-		part_file_name = TensorFlowDataset.tensorflow_file_name(dataset_dir, 'part')
-		return(part_file_name)
+    def _part_file_name(self, dataset_dir):
+        part_file_name = TensorFlowDataset.tensorflow_file_name(
+            dataset_dir, 'part')
+        return (part_file_name)
 
-	def _negative_file_name(self, dataset_dir):
-		negative_file_name = TensorFlowDataset.tensorflow_file_name(dataset_dir, 'negative')
-		return(negative_file_name)
+    def _negative_file_name(self, dataset_dir):
+        negative_file_name = TensorFlowDataset.tensorflow_file_name(
+            dataset_dir, 'negative')
+        return (negative_file_name)
 
-	def _image_list_file_name(self, dataset_dir):
-		image_list_file_name = TensorFlowDataset.tensorflow_file_name(dataset_dir, 'image_list')
-		return(image_list_file_name)
+    def _image_list_file_name(self, dataset_dir):
+        image_list_file_name = TensorFlowDataset.tensorflow_file_name(
+            dataset_dir, 'image_list')
+        return (image_list_file_name)
 
-	def train(self, network_name, dataset_root_dir, train_root_dir, base_learning_rate, learning_rate_epoch, max_number_of_epoch, log_every_n_steps):
-		raise NotImplementedError('Must be implemented by the subclass.')
-
+    def train(self, network_name, dataset_root_dir, train_root_dir,
+              base_learning_rate, learning_rate_epoch, max_number_of_epoch,
+              log_every_n_steps):
+        raise NotImplementedError('Must be implemented by the subclass.')
--- tfmtcnn/trainers/ModelEvaluator.py
+++ tfmtcnn/trainers/ModelEvaluator.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2018
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,64 +37,67 @@ import tfmtcnn.datasets.constants as datasets_constants
 from tfmtcnn.utils.convert_to_square import convert_to_square
 from tfmtcnn.utils.IoU import IoU
 
+
 class ModelEvaluator(object):
+    def __init__(self):
+        self._test_dataset = None
+        self._face_detector = None
 
-	def __init__(self):
-		self._test_dataset = None
-		self._face_detector = None
+    def load(self, dataset_name, annotation_image_dir, annotation_file_name):
+        face_dataset = DatasetFactory.face_dataset(dataset_name)
+        if (not face_dataset):
+            return (False)
 
-	def load(self, dataset_name, annotation_image_dir, annotation_file_name):
-		face_dataset = DatasetFactory.face_dataset(dataset_name)
-		if(not face_dataset):
-			return(False)
+        if (not face_dataset.read(annotation_image_dir, annotation_file_name)):
+            return (False)
 
-		if(not face_dataset.read(annotation_image_dir, annotation_file_name)):						
-			return(False)
+        self._test_dataset = face_dataset.data()
+        return (True)
 
-		self._test_dataset = face_dataset.data()
-		return(True)				
+    def create_detector(self, last_network, model_root_dir):
+        self._face_detector = FaceDetector(last_network, model_root_dir)
 
-	def create_detector(self, last_network, model_root_dir):
-		self._face_detector = FaceDetector(last_network, model_root_dir)
+        minimum_face_size = datasets_constants.minimum_face_size
+        self._face_detector.set_min_face_size(minimum_face_size)
+        return (True)
 
-		minimum_face_size = datasets_constants.minimum_face_size
-		self._face_detector.set_min_face_size(minimum_face_size)
-		return(True)
+    def evaluate(self, print_result=False):
+        if (not self._test_dataset):
+            return (False)
 
-	def evaluate(self, print_result=False):
-		if(not self._test_dataset):
-			return(False)
+        if (not self._face_detector):
+            return (False)
 
-		if(not self._face_detector):
-			return(False)
+        test_data = InferenceBatch(self._test_dataset['images'])
+        detected_boxes, landmarks = self._face_detector.detect_face(test_data)
 
-		test_data = InferenceBatch(self._test_dataset['images'])
-		detected_boxes, landmarks = self._face_detector.detect_face(test_data)
+        image_file_names = self._test_dataset['images']
+        ground_truth_boxes = self._test_dataset['bboxes']
+        number_of_images = len(image_file_names)
 
-		image_file_names = self._test_dataset['images']
-		ground_truth_boxes = self._test_dataset['bboxes']
-		number_of_images = len(image_file_names)
+        if (not (len(detected_boxes) == number_of_images)):
+            return (False)
 
-		if(not (len(detected_boxes) == number_of_images)):
-			return(False)
+        number_of_positive_faces = 0
+        number_of_part_faces = 0
+        number_of_ground_truth_faces = 0
+        accuracy = 0.0
+        for image_file_path, detected_box, ground_truth_box in zip(
+                image_file_names, detected_boxes, ground_truth_boxes):
+            ground_truth_box = np.array(
+                ground_truth_box, dtype=np.float32).reshape(-1, 4)
+            number_of_ground_truth_faces = number_of_ground_truth_faces + len(
+                ground_truth_box)
+            if (detected_box.shape[0] == 0):
+                continue
 
-		number_of_positive_faces = 0
-		number_of_part_faces = 0  
-		number_of_ground_truth_faces = 0
-		accuracy = 0.0
-		for image_file_path, detected_box, ground_truth_box in zip(image_file_names, detected_boxes, ground_truth_boxes):
-       			ground_truth_box = np.array(ground_truth_box, dtype=np.float32).reshape(-1, 4)
-			number_of_ground_truth_faces = number_of_ground_truth_faces + len(ground_truth_box)
-	       		if( detected_box.shape[0] == 0 ):
-            			continue
+            detected_box = convert_to_square(detected_box)
+            detected_box[:, 0:4] = np.round(detected_box[:, 0:4])
 
-       			detected_box = convert_to_square(detected_box)
-       			detected_box[:, 0:4] = np.round(detected_box[:, 0:4])
+            current_image = cv2.imread(image_file_path)
 
-			current_image = cv2.imread(image_file_path)
-
-       			for box in ground_truth_box:
-				"""
+            for box in ground_truth_box:
+                """
        				x_left, y_top, x_right, y_bottom, _ = box.astype(int)
        				width = x_right - x_left + 1
        				height = y_bottom - y_top + 1
@@ -103,23 +106,27 @@ class ModelEvaluator(object):
                				continue
 				"""
 
-				current_IoU = IoU(box, detected_box)
-				maximum_IoU = np.max(current_IoU)
+                current_IoU = IoU(box, detected_box)
+                maximum_IoU = np.max(current_IoU)
 
-				accuracy = accuracy + maximum_IoU
-				if(maximum_IoU > datasets_constants.positive_IoU):
-					number_of_positive_faces = number_of_positive_faces + 1
-				elif (maximum_IoU > datasets_constants.part_IoU):
-					number_of_part_faces = number_of_part_faces + 1
+                accuracy = accuracy + maximum_IoU
+                if (maximum_IoU > datasets_constants.positive_IoU):
+                    number_of_positive_faces = number_of_positive_faces + 1
+                elif (maximum_IoU > datasets_constants.part_IoU):
+                    number_of_part_faces = number_of_part_faces + 1
 
-		if(print_result):
-			print('Positive faces       - ', number_of_positive_faces)
-			print('Partial faces        - ', number_of_part_faces)
-			print('Total detected faces - ', (number_of_positive_faces + number_of_part_faces))
-			print('Ground truth faces   - ', number_of_ground_truth_faces)
-			print('Positive accuracy    - ', number_of_positive_faces/number_of_ground_truth_faces)
-			print('Detection accuracy   - ', (number_of_positive_faces + number_of_part_faces)/number_of_ground_truth_faces)
-			print('Accuracy             - ', accuracy/number_of_ground_truth_faces)
+        if (print_result):
+            print('Positive faces       - ', number_of_positive_faces)
+            print('Partial faces        - ', number_of_part_faces)
+            print('Total detected faces - ',
+                  (number_of_positive_faces + number_of_part_faces))
+            print('Ground truth faces   - ', number_of_ground_truth_faces)
+            print('Positive accuracy    - ',
+                  number_of_positive_faces / number_of_ground_truth_faces)
+            print('Detection accuracy   - ',
+                  (number_of_positive_faces + number_of_part_faces) /
+                  number_of_ground_truth_faces)
+            print('Accuracy             - ',
+                  accuracy / number_of_ground_truth_faces)
 
-		return(True)
-
+        return (True)
