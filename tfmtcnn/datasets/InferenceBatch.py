594b5d3ce5d8b3881cfe0c2d8d81e4da2125c652
look4pritam <pritam.shete@gmail.com>
2018-12-24 09:19:58 +0530

    Code is formatted.

--- tfmtcnn/datasets/InferenceBatch.py
+++ tfmtcnn/datasets/InferenceBatch.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2018
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,57 +26,56 @@ from __future__ import print_function
 
 import cv2
 
+
 class InferenceBatch(object):
+    def __init__(self, images, batch_size=1, shuffle=False):
+        self.images = images
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.size = len(self.images)
 
-    	def __init__(self, images, batch_size=1, shuffle=False):
-        	self.images = images
-        	self.batch_size = batch_size
-        	self.shuffle = shuffle
-        	self.size = len(self.images)
-        	
-        	self.current = 0
-        	self.data = None
-        	self.label = None
+        self.current = 0
+        self.data = None
+        self.label = None
 
-        	self.reset()
-        	self.get_batch()
+        self.reset()
+        self.get_batch()
 
-    	def reset(self):
-        	self.current = 0
-        	if( self.shuffle ):
-        	    np.random.shuffle(self.images)
+    def reset(self):
+        self.current = 0
+        if (self.shuffle):
+            np.random.shuffle(self.images)
 
-    	def has_next(self):
-        	return( self.current + self.batch_size <= self.size )
+    def has_next(self):
+        return (self.current + self.batch_size <= self.size)
 
-    	def __iter__(self):
-        	return( self )
-    
-    	def __next__(self):
-        	return( self.next() )
+    def __iter__(self):
+        return (self)
 
-    	def next(self):
-        	if( self.has_next() ):
-            		self.get_batch()
-            		self.current += self.batch_size
-            		return( self.data )
-        	else:
-            		raise StopIteration
+    def __next__(self):
+        return (self.next())
 
-    	def getindex(self):
-        	return( self.current / self.batch_size )
+    def next(self):
+        if (self.has_next()):
+            self.get_batch()
+            self.current += self.batch_size
+            return (self.data)
+        else:
+            raise StopIteration
 
-    	def getpad(self):
-        	if( self.current + self.batch_size > self.size ):
-            		return( self.current + self.batch_size - self.size )
-        	else:
-            		return( 0 )
+    def getindex(self):
+        return (self.current / self.batch_size)
 
-    	def get_batch(self):
-        	image_path = self.images[self.current]
-        	input_bgr_image = cv2.imread(image_path)
+    def getpad(self):
+        if (self.current + self.batch_size > self.size):
+            return (self.current + self.batch_size - self.size)
+        else:
+            return (0)
 
-		input_rgb_image = cv2.cvtColor(input_bgr_image, cv2.COLOR_BGR2RGB)
-        	self.data = input_rgb_image
-		#self.data = input_bgr_image
+    def get_batch(self):
+        image_path = self.images[self.current]
+        input_bgr_image = cv2.imread(image_path)
 
+        input_rgb_image = cv2.cvtColor(input_bgr_image, cv2.COLOR_BGR2RGB)
+        self.data = input_rgb_image
+        #self.data = input_bgr_image
