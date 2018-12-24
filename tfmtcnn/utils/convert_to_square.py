865ad0d86e6356bca5f56642a8e752842f76b4c2
look4pritam <pritam.shete@gmail.com>
2018-12-24 09:27:11 +0530

    Code is formatted.

--- tfmtcnn/utils/convert_to_square.py
+++ tfmtcnn/utils/convert_to_square.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2018
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,14 +26,14 @@ from __future__ import print_function
 
 import numpy as np
 
-def convert_to_square(bbox): 
-        square_bbox = bbox.copy()
-        h = bbox[:, 3] - bbox[:, 1] + 1
-        w = bbox[:, 2] - bbox[:, 0] + 1
-        max_side = np.maximum(h, w)
-        square_bbox[:, 0] = bbox[:, 0] + w * 0.5 - max_side * 0.5
-        square_bbox[:, 1] = bbox[:, 1] + h * 0.5 - max_side * 0.5
-        square_bbox[:, 2] = square_bbox[:, 0] + max_side - 1
-        square_bbox[:, 3] = square_bbox[:, 1] + max_side - 1
-        return( square_bbox )
 
+def convert_to_square(bbox):
+    square_bbox = bbox.copy()
+    h = bbox[:, 3] - bbox[:, 1] + 1
+    w = bbox[:, 2] - bbox[:, 0] + 1
+    max_side = np.maximum(h, w)
+    square_bbox[:, 0] = bbox[:, 0] + w * 0.5 - max_side * 0.5
+    square_bbox[:, 1] = bbox[:, 1] + h * 0.5 - max_side * 0.5
+    square_bbox[:, 2] = square_bbox[:, 0] + max_side - 1
+    square_bbox[:, 3] = square_bbox[:, 1] + max_side - 1
+    return (square_bbox)
