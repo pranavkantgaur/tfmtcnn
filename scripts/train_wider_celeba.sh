5dee705a5f7d96f65f29955467cf7783d06407ed
look4pritam <pritam.shete@gmail.com>
2019-01-21 20:20:42 +0530

    New training script is added.
    New training script is added.

--- scripts/train_wider_celeba.sh
+++ scripts/train_wider_celeba.sh
@@ -22,7 +22,7 @@ echo '##########################################################################
 
 echo 'Prepare CelebA dataset for input.'
 
-python tools/prepare_celeba_dataset.py --bounding_box_file_name ../data/CelebA/list_bbox_celeba.txt --landmark_file_name ../data/CelebA/list_landmarks_celeba.txt --output_file_name ../data/CelebA/CelebA.txt 
+python tools/prepare_celeba_dataset.py --bounding_box_file_name=../data/CelebA/list_bbox_celeba.txt --landmark_file_name=../data/CelebA/list_landmarks_celeba.txt --output_file_name=../data/CelebA/CelebA.txt
 
 echo 'Prepared CelebA dataset for input.'
 
@@ -30,7 +30,7 @@ echo '##########################################################################
 
 echo 'Generate a basic dataset i.e. PNet dataset.'
 
-python generate_simple_dataset.py --annotation_image_dir ../data/WIDER_Face/WIDER_train/images --annotation_file_name ../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir ../data/CelebA/images --landmark_file_name ../data/CelebA/CelebA.txt --base_number_of_images 700000 --target_root_dir ../data/datasets/mtcnn 
+python generate_simple_dataset.py --annotation_image_dir ../data/WIDER_Face/WIDER_train/images --annotation_file_name ../data/WIDER_Face/WIDER_train/wider_face_train_bbx_gt.txt --landmark_image_dir ../data/CelebA/images --landmark_file_name ../data/CelebA/CelebA.txt --base_number_of_images 700000 --target_root_dir ../data/datasets/mtcnn
 
 echo 'Generated a basic dataset i.e. PNet dataset.'
 
@@ -38,7 +38,7 @@ echo '##########################################################################
 
 echo 'Train PNet.'
 
-python train_model.py --network_name PNet --train_root_dir ../data/models/mtcnn/train --dataset_root_dir ../data/datasets/mtcnn --base_learning_rate 0.01 --learning_rate_epoch 8 16 24 --max_number_of_epoch 32
+python train_model.py --network_name PNet --train_root_dir ../data/models/mtcnn/train --dataset_root_dir ../data/datasets/mtcnn --base_learning_rate 0.001 --max_number_of_epoch 19 --test_dataset FDDBDataset --test_annotation_image_dir /datasets/FDDB/ --test_annotation_file /datasets/FDDB/FDDB-folds/FDDB-fold-01-ellipseList.txt
 
 echo 'Trained PNet.'
 
@@ -54,7 +54,7 @@ echo '##########################################################################
 
 echo 'Train RNet.'
 
-python train_model.py --network_name RNet --train_root_dir ../data/models/mtcnn/train --dataset_root_dir ../data/datasets/mtcnn --base_learning_rate 0.01 --learning_rate_epoch 8 16 24 --max_number_of_epoch 32
+python train_model.py --network_name RNet --train_root_dir ../data/models/mtcnn/train --dataset_root_dir ../data/datasets/mtcnn --base_learning_rate 0.001 --max_number_of_epoch 22 --test_dataset FDDBDataset --test_annotation_image_dir /datasets/FDDB/ --test_annotation_file /datasets/FDDB/FDDB-folds/FDDB-fold-01-ellipseList.txt
 
 echo 'Trained RNet.'
 
@@ -70,7 +70,7 @@ echo '##########################################################################
 
 echo 'Train ONet.'
 
-python train_model.py --network_name ONet --train_root_dir ../data/models/mtcnn/train --dataset_root_dir ../data/datasets/mtcnn --base_learning_rate 0.01 --learning_rate_epoch 8 16 24 --max_number_of_epoch 32
+python train_model.py --network_name ONet --train_root_dir ../data/models/mtcnn/train --dataset_root_dir ../data/datasets/mtcnn --base_learning_rate 0.001 --max_number_of_epoch 21 --test_dataset FDDBDataset --test_annotation_image_dir /datasets/FDDB/ --test_annotation_file /datasets/FDDB/FDDB-folds/FDDB-fold-01-ellipseList.txt
 
 echo 'Trained ONet.'
 
